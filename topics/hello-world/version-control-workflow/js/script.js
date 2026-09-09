148fc310cce65210c55028afdca641e0f1b0cdf1
FridWright <fridwright@yahoo.ca>
2026-09-09 16:54:27 -0400

    Pasted in the example code for the yellow pyramid/tunnel and red circle

--- topics/hello-world/version-control-workflow/js/script.js
+++ topics/hello-world/version-control-workflow/js/script.js
@@ -26,11 +26,11 @@ function setup() {
 */
 function draw() {
     // Make the background black (specified as RGB)
-    background(2, 60, 300);
+    background(0, 0, 0);
 
     // Draw a pyramid
     // How many levels for the pyramid
-    const levels = 100;
+    const levels = 10;
     // Loop through every level (backwards)
     for (let level = levels; level > 0; level--) {
         // Draw this layer
@@ -38,7 +38,7 @@ function draw() {
         // Set the grey shade of the level based on its number
         // e.g. level 1 will get a shade of 10 (dark gray), 
         // level 10 will be 255(white)
-        const shade = map(level, 100, levels, 300, 255);
+        const shade = map(level, 1, levels, 10, 255);
         // No line around the levels
         noStroke();
         // Set the fill colour to our shade (RGB)
@@ -49,7 +49,7 @@ function draw() {
         // (320, 320) with a size based on the level
         // e.g. level 1 will be a 48x48 rectangle and
         // level 10 will be a 480x480 rectangle
-        rect(120, 720, level * 38, level * 18);
+        rect(320, 320, level * 48, level * 48);
         pop();
     }
 
@@ -58,7 +58,7 @@ function draw() {
     // No line around the shape
     noStroke();
     // Make it red (RGB)
-    fill(20, 70, 90);
+    fill(255, 0, 0);
     // Draw a 100x100 circle at the mouse position
     ellipse(mouseX, mouseY, 100, 100);
     pop();

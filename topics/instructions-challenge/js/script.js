68f725bfe17c6cf70d0ce2d47b5e1ea7836f9f00
FridWright <fridwright@yahoo.ca>
2026-09-16 18:23:21 -0400

    created a dark blue background and added a circle shape

--- topics/instructions-challenge/js/script.js
+++ topics/instructions-challenge/js/script.js
@@ -12,6 +12,9 @@
 */
 function setup() {
 
+    // Creates a canvas 800 pixels wide and 600 pixels high.
+    createCanvas(800, 600);
+
 }
 
 
@@ -20,4 +23,9 @@ function setup() {
 */
 function draw() {
 
+    // Make a dark blue background.
+    background(0, 0, 50);
+
+    // Large circle as a lunar surface at bottom middle.
+    circle(400, 700, 700);
 }

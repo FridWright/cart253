79f9eb56b9550d53acaafa46f5035e76c2d2b9a8
FridWright <fridwright@yahoo.ca>
2026-09-23 16:58:12 -0400

    created and filled other 3 rectangles to have a quadrant

--- topics/prototyping-instructions/2-prototyping-instructions/js/script.js
+++ topics/prototyping-instructions/2-prototyping-instructions/js/script.js
@@ -26,14 +26,28 @@ function setup() {
 function draw() {
 
     // Draw rectangle 1 (upper left)
-    rect(200, 300, 100)
-    stroke(255, 255, 255)
-    strokeWeight(5)
-    fill('orange')
+    stroke(255, 255, 255);
+    strokeWeight(5);
+    fill('orange');
+    rect(0, 0, width / 2, height / 2);
+
     // Draw rectangle 2 (upper right)
+    stroke(255, 255, 255);
+    strokeWeight(5);
+    fill('blue');
+    rect(width / 2, 0, width / 2, height / 2);
 
     // Draw rectangle 3 (lower left)
+    stroke(255, 255, 255);
+    strokeWeight(5);
+    fill('green');
+    rect(0, height / 2, width / 2, height / 2);
+
 
     // Draw rectangle 4 (lower right)
+    stroke(255, 255, 255);
+    strokeWeight(5);
+    fill('pink');
+    rect(width / 2, height / 2, width / 2, height / 2);
 
 }
